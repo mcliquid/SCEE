e7b9ec4d22733b54639cc7a40dc2bdb1fb93e04f
mcliquid <info@mcliquid.de>
2024-09-15 18:52:02 +0200

    Add Sauna Availability Quest

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/sauna_availability/AddSaunaAvailability.kt
@@ -0,0 +1,32 @@
+package de.westnordost.streetcomplete.quests.sauna_availability
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
+import de.westnordost.streetcomplete.osm.Tags
+import de.westnordost.streetcomplete.quests.YesNoQuestForm
+import de.westnordost.streetcomplete.util.ktx.toYesNo
+
+class AddSaunaAvailability : OsmFilterQuestType<Boolean>() {
+
+    override val elementFilter = """
+        nodes, ways with
+        (
+          leisure ~ fitness_centre|resort|sports_centre|water_park
+          or leisure = sports_hall and sport = swimming
+          or tourism ~ camp_site|hotel
+        )
+        and !sauna
+    """
+    override val changesetComment = "Survey sauna availabilities"
+    override val wikiLink = "Key:sauna"
+    override val icon = R.drawable.ic_quest_sauna
+
+    override fun getTitle(tags: Map<String, String>) = R.string.quest_saunaAvailability_title
+
+    override fun createForm() = YesNoQuestForm()
+
+    override fun applyAnswerTo(answer: Boolean, tags: Tags, geometry: ElementGeometry, timestampEdited: Long) {
+        tags["sauna"] = answer.toYesNo()
+    }
+}

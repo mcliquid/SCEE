7d9e4529ab35ae0fc819b568065bce30ba1d2cd2
mcliquid <info@mcliquid.de>
2025-05-28 08:18:38 +0200

    add interior_decoration to Places Overlay

--- app/src/main/java/de/westnordost/streetcomplete/osm/Place.kt
+++ app/src/main/java/de/westnordost/streetcomplete/osm/Place.kt
@@ -367,6 +367,7 @@ val KEYS_THAT_SHOULD_BE_REMOVED_WHEN_PLACE_IS_REPLACED = listOf(
     "engineering", "forestry", "foundation", "lawyer", "logistics", "military", "community_centre",
     "bank", "operational", "users_(PLWD|boy|elderly|female|girl|men)",
     "Comments?", "comments?", "entrance:(width|step_count|kerb:height)", "fenced", "motor_vehicle",
+    "interior_decoration",
 )
     .flatMap { listOf(it, "source:$it", "check_date:$it") }
     .map { it.intern().toRegex() }

b67f8f0d9fc5052bc497681866a42daa95013165
mcliquid <info@mcliquid.de>
2026-04-24 12:21:51 +0200

    make it work

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/util/Winter.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/util/Winter.kt
@@ -3,13 +3,27 @@ package de.westnordost.streetcomplete.util
 import de.westnordost.streetcomplete.data.osm.mapdata.LatLon
 import de.westnordost.streetcomplete.util.ktx.systemTimeNow
 import de.westnordost.streetcomplete.util.ktx.toLocalDate
-import java.time.Month.*
+import kotlinx.datetime.Month
 
 fun isWinter(location: LatLon?): Boolean {
     if (location == null) return false
     val now = systemTimeNow().toLocalDate()
-    val winterSeason = if (location.latitude > 0)
-        listOf(NOVEMBER, DECEMBER, JANUARY, FEBRUARY, MARCH, APRIL)
-    else listOf(JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER)
+    val winterSeason: List<Month> = if (location.latitude > 0)
+        listOf(
+            Month.NOVEMBER,
+            Month.DECEMBER,
+            Month.JANUARY,
+            Month.FEBRUARY,
+            Month.MARCH,
+            Month.APRIL
+        )
+    else
+        listOf(
+            Month.JUNE,
+            Month.JULY,
+            Month.AUGUST,
+            Month.SEPTEMBER,
+            Month.OCTOBER
+        )
     return now.month in winterSeason
 }
